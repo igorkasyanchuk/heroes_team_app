355dadeddb8ad6cd2889e119e97b157ab9713e5a
Anton Chupak <antonchupack@gmail.com>
2017-12-19 21:04:58 +0200

    [~]Refactored a bit spec/rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,9 +76,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-end
 
-# For using the `Devise::Test::ControllerHelpers`
-RSpec.configure do |config|
+  # For using the `Devise::Test::ControllerHelpers`
   config.include Devise::Test::ControllerHelpers, type: :controller
 end
