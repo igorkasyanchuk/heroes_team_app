632b469bc3dbd686d316d997b6b7250b5fcc7a1f
Oleksandr Adamchuk <stranger.if.2014@gmail.com>
2017-12-27 15:04:24 +0000

    Fixed code style discrepancy

--- app/controllers/account/dashboard_controller.rb
+++ app/controllers/account/dashboard_controller.rb
@@ -1,7 +1,13 @@
 class Account::DashboardController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
-    render :layout => "dashboard"
+    render layout: dashboard
+  end
+
+  private
+
+  def user_admin?
+    current_user.role.eql? "admin"
   end
 end
--- spec/controllers/account/dashboard_controller_spec.rb
+++ spec/controllers/account/dashboard_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Account::DashboardController, type: :controller do
-
+  describe "GET #index" do
+    it "renders the :index view" do
+      get :index
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:index)
+    end
+  end
 end
