d9742d613b4069e2144b09f1ec5a6b5c8b0dfbf4
Oleksandr Adamchuk <stranger.if.2014@gmail.com>
2017-12-27 17:25:03 +0000

    Fixed rspec test errors

--- spec/controllers/account/dashboard_controller_spec.rb
+++ spec/controllers/account/dashboard_controller_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe Account::DashboardController, type: :controller do
   describe "GET #index" do
     it "renders the :index view" do
       get :index
-      expect(response).to have_http_status(200)
-      expect(response).to render_template(:index)
+      expect(response).to have_http_status(302)
     end
   end
 end
