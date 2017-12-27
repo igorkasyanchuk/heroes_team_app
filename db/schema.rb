f8954c38a3affba25893123a22529345b32af23c
Misha <misha.dovganych@gmail.com>
2017-12-27 18:00:09 +0200

    Modified gemfile.lock

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20171225200830) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "pages", force: :cascade do |t|
     t.string "page_type"
     t.string "title"
