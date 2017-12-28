c40d9459c7cd0cec951ca6b0ca7fd5773c5a55b6
Ivan Pushkedra <pushkedraid@gmail.com>
2017-12-28 08:35:42 +0200

    Added validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   ADMIN_ROLE = 'admin'.freeze
   MODERATOR_ROLE = 'moderator'.freeze
   ROLES = [SALE_ROLE, ADMIN_ROLE, MODERATOR_ROLE].freeze
+<<<<<<< HEAD
   DEFAULT_PASSWORD = 'password'.freeze
+=======
+  VALID_PHONE_REGEX = /\A[+]?[\d\-.() ]+\z/
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+>>>>>>> Added validation for user
 
   has_many :companies, dependent: :destroy
   belongs_to :tenant, optional: true
@@ -16,11 +21,16 @@ class User < ApplicationRecord
   validates :first_name, length: { minimum: 3, maximum: 50 }, presence: true
   validates :last_name,  length: { minimum: 3, maximum: 50 }, presence: true
   validates :tenant, presence: true
+<<<<<<< HEAD
   validates :role, presence: true, inclusion: ROLES
 
   scope :by_date, -> { order(created_at: :asc) }
 
   delegate :name, to: :tenant, prefix: true, allow_nil: true
+=======
+  validates :phone, length: { maximum: 32 }, format: { with: VALID_PHONE_REGEX }, allow_blank: true
+  validates :email, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
+>>>>>>> Added validation for user
 
   def full_name
     [first_name, last_name].reject(&:blank?).join(' ')
