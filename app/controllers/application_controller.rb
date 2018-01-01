bf22b387684529975320ef6e18fecb491b5e76d8
Anton Chupak <antonchupack@gmail.com>
2018-01-01 21:45:05 +0200

    Added landing layout for sign in&up. improved pathes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  layout :layout_by_resource
+
   def current_tenant
     current_user&.tenant
   end
 
   def authorize_admin!
     redirect_to(root_path) unless current_user&.role == User::ADMIN_ROLE
+  end
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :user && action_name == 'new'
+      "landing"
+    else
+      "application"
+    end
   end
 end
