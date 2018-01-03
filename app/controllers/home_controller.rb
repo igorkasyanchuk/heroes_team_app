801324e680402961de3972564e85d1ca4a8d83b6
Oleksandr Adamchuk <stranger.if.2014@gmail.com>
2018-01-03 14:01:36 +0000

    Fixed views and routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,3 @@
 class HomeController < ApplicationController
-  def index
-    redirect_to account_root_path if user_signed_in?
-  end
+  def index;  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  namespace :account do
-    root 'dashboard#index'
-  end
-
   root 'home#index'
   devise_for :users, path: 'account', controllers: {
     registrations: 'users/registrations'
   }
 
   namespace :account do
+    root 'dashboard#index'
     resources :companies do
       resources :pages, only: %i[show index]
       get :download, on: :member
